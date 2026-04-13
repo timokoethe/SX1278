b3d0830b193cca3da1e0076def78dc5446563284
Timo Köthe <timo.info@t-online.de>
2026-04-13 17:44:31 +0200

    refactor: clean up examples and sx1278 API
    Rename pins and tidy example scripts, plus minor API and style cleanup in sx1278.py. In examples: rename RS to RST, reformat SPI initialization, introduce MESSAGE constant in send.py, rename onReceive->on_receive and ensure lr.recv() is called via try/finally, and use the MESSAGE constant when sending. In sx1278.py: remove unused imports (struct, ubinascii), rename set_tx_power parameter to snake_case (output_pin), fix indentation/tab issues, and minor spacing/formatting tweaks. These are non-functional style and robustness improvements to make the code more consistent and maintainable.

--- example/receive.py
+++ example/receive.py
@@ -1,38 +1,34 @@
 from sx1278 import Lora
 from machine import Pin, SPI
-from time import sleep
 
-# Setup Pins
 SCK  = 2
 MOSI = 3
 MISO = 0
 CS   = 1
 RX   = 10
-RS   = 16
+RST  = 16
 
-# Setup SPI
-spi = SPI(0, baudrate=10000000,
-          sck=Pin(SCK, Pin.OUT, Pin.PULL_DOWN),
-          mosi=Pin(MOSI, Pin.OUT, Pin.PULL_UP),
-          miso=Pin(MISO, Pin.IN, Pin.PULL_UP))
+spi = SPI(
+    0,
+    baudrate=10000000,
+    sck=Pin(SCK, Pin.OUT, Pin.PULL_DOWN),
+    mosi=Pin(MOSI, Pin.OUT, Pin.PULL_UP),
+    miso=Pin(MISO, Pin.IN, Pin.PULL_UP))
 spi.init()
 
-# Setup LoRa
 lr = Lora(
     spi,
     cs=Pin(CS, Pin.OUT),
     rx=Pin(RX, Pin.IN),
-    rs=Pin(RS, Pin.OUT))
+    rs=Pin(RST, Pin.OUT))
 
-# Receive handler
-def onReceive(x):
-    print(x)
-    # Put module back in receiving mode
-    lr.recv()
+def on_receive(message):
+    try:
+        print(message)
+    finally:
+        lr.recv()
 
-# Set handler
-lr.on_recv(onReceive)
-# Put module in receiving mode
+lr.on_recv(on_receive)
 lr.recv()
 
 while True:
--- example/send.py
+++ example/send.py
@@ -2,29 +2,30 @@ from sx1278 import Lora
 from machine import Pin, SPI
 from time import sleep
 
-# etup Pins
 SCK  = 2
 MOSI = 3
 MISO = 0
 CS   = 1
 RX   = 10
-RS   = 16
+RST  = 16
 
-# Setup SPI
-spi = SPI(0, baudrate=10000000,
-          sck=Pin(SCK, Pin.OUT, Pin.PULL_DOWN),
-          mosi=Pin(MOSI, Pin.OUT, Pin.PULL_UP),
-          miso=Pin(MISO, Pin.IN, Pin.PULL_UP))
+MESSAGE = "Hello World!"
+
+spi = SPI(
+    0, 
+    baudrate=10000000,
+    sck=Pin(SCK, Pin.OUT, Pin.PULL_DOWN),
+    mosi=Pin(MOSI, Pin.OUT, Pin.PULL_UP),
+    miso=Pin(MISO, Pin.IN, Pin.PULL_UP))
 spi.init()
 
-# Setup LoRa
 lr = Lora(
     spi,
     cs=Pin(CS, Pin.OUT),
     rx=Pin(RX, Pin.IN),
-    rs=Pin(RS, Pin.OUT))
+    rs=Pin(RST, Pin.OUT))
 
 while True:
-    lr.send('Hello world!')
-    print('Sent: Hello world!')
+    lr.send(MESSAGE)
+    print('Sent:', MESSAGE)
     sleep(1)
